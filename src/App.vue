c583d56cef2ed7a2f090eaad69646abb40169374
AndreuRosellOsuna <andreu.rosell4@gmail.com>
2025-12-23 12:57:10 +0100

    Add new ideas

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup>
-//
 import Column from "@/components/Column.vue";
 import AppFooter from "@/components/AppFooter.vue";
 
 import {useRetroIdeasStore} from "@/stores/retroIdeas.js";
 import {storeToRefs} from "pinia";
-import column from "@/components/Column.vue";
 
 const ideasStore = useRetroIdeasStore()
 const {thingsWentWell, thingsWentNotSoWell, feelings} = storeToRefs(ideasStore)
 
-function addNewIdea(){
-  ideasStore.addIdea(column, "New idea")
+function addNewIdea(column, ideaText){
+  ideasStore.addIdea(column, ideaText)
 }
 </script>
 
 <template>
   <v-app>
     <v-main>
-<!--      <HelloWorld />-->
-      <v-container class="fill-height" max-width="" style="background-color: aquamarine">
+      <v-container class="fill-height" max-width="" style="background-color: beige">
           <v-row  align="start">
 
-            <Column column="TWW" title="Things that went well" :ideas="thingsWentWell" style="background-color: blueviolet">Things went well</Column>
-            <Column column="TWNSW" title="Things that went not so well" :ideas="thingsWentNotSoWell" style="background-color: burlywood">Things went not so well</Column>
-            <Column column="FF" title="Feelings" :ideas="feelings" style="background-color: darkslategray">Feelings</Column>
+            <Column column="TWW"
+                    title="Things that went well"
+                    dialog-title="Add something that went so well that we should do more of it"
+                    :ideas="thingsWentWell"
+                    @add-new-idea="addNewIdea"
+                    style="background-color: green">
+            </Column>
+            <Column column="TWNSW"
+                    title="Things that went not so well"
+                    dialog-title="Add something that went not so well that we should stop doing it"
+                    :ideas="thingsWentNotSoWell"
+                    @add-new-idea="addNewIdea"
+                    style="background-color: orangered">
+
+            </Column>
+            <Column column="FF"
+                    title="Feelings"
+                    dialog-title="Add a feeling that you have"
+                    :ideas="feelings"
+                    @add-new-idea="addNewIdea"
+                    style="background-color: dodgerblue">
+            </Column>
 
           </v-row>
       </v-container>
--- src/components/Column.vue
+++ src/components/Column.vue
@@ -1,13 +1,12 @@
 <script setup>
 
-import {useRetroIdeasStore} from "@/stores/retroIdeas.js";
-import column from "@/components/Column.vue";
+import {ref} from "vue";
 
-
-
-defineProps({
+const emit = defineEmits(['addNewIdea'])
+const { column } = defineProps({
   column: String,
   title: String,
+  dialogTitle: String,
   ideas: [{
     id: Number,
     column: 'TWW' | 'TWNSW' | 'FF',
@@ -15,17 +14,47 @@ defineProps({
   }]
 })
 
+const openAddIdeaDialog = ref(false)
+const newIdeaValue = ref('')
+const newIdeaMaxValue = [v => v.length < 100 || 'Max 100 characters']
+
+function cleanAndCloseDialog() {
+  openAddIdeaDialog.value = false
+  newIdeaValue.value = ''
+}
+
+function cancelDialog() {
+  cleanAndCloseDialog()
+}
 
 function addNewIdea(){
-  // ideasStore.addIdea(column, "New idea")
-  //TODO add event to push new idea
+  emit('addNewIdea', column, newIdeaValue.value)
+  cleanAndCloseDialog()
 }
 
 </script>
 
 <template>
   <v-col >
-    <h2 @click="addNewIdea">{{ title }}</h2>
+    <div class="title" @click="openAddIdeaDialog = true" >
+      <h2><v-icon color="black" icon="mdi-plus-circle"></v-icon >{{ title }}</h2>
+      <v-dialog max-width="700" v-model="openAddIdeaDialog" persistent>
+        <template v-slot:default>
+          <v-card :title="dialogTitle" prepend-icon="mdi-information-variant-box">
+            <v-card-text>
+              <v-textarea label="Add your new idea here" v-model="newIdeaValue" :rules="newIdeaMaxValue" counter autofocus></v-textarea>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text="Cancel" color="red" variant="tonal" @click="cancelDialog"></v-btn>
+              <v-btn text="Save idea" color="success" variant="tonal" @click="addNewIdea"></v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+
+    </div>
     <v-sheet class="pa-2 ma-2" border rounded :elevation="12">
       <v-list v-if="ideas.length > 0">
         <v-list-item v-for="idea in ideas" :title="idea.text">
@@ -35,3 +64,14 @@ function addNewIdea(){
     </v-sheet>
   </v-col>
 </template>
+
+<style scoped>
+h2 {
+  cursor: pointer;
+}
+
+.title {
+  margin-left: 10px;
+}
+
+</style>
