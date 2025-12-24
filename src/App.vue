8f8de224548a05925baf68b6fe859c086c9f2eb1
AndreuRosellOsuna <andreu.rosell4@gmail.com>
2025-12-24 12:34:43 +0100

    Refactor root component to the main component

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,11 @@
 <script setup>
-import Column from "@/components/Column.vue";
 import AppFooter from "@/components/AppFooter.vue";
-import {useRetroIdeasStore} from "@/stores/retroIdeas.js";
-import {storeToRefs} from "pinia";
-import {onMounted, onUnmounted} from "vue";
-import pocketbase from "@/plugins/pocketbase.js";
-
-const ideasStore = useRetroIdeasStore()
-const {thingsWentWell, thingsWentNotSoWell, feelings} = storeToRefs(ideasStore)
-
-let ideasSubscription
-
-onMounted(() => {
-  ideasStore.refreshIdeas()
-  ideasSubscription = pocketbase.collection("ideas").subscribe("*", event => {
-    ideasStore.refreshIdeas()
-  })
-})
-
-onUnmounted(() => ideasSubscription.unsubscribe())
-
-function addNewIdea(column, ideaText){
-  ideasStore.addIdea(column, ideaText)
-}
+import AppMain from "@/components/AppMain.vue";
 </script>
 
 <template>
   <v-app>
-    <v-main>
-      <v-container class="fill-height" max-width="" style="background-color: beige">
-          <v-row  align="start">
-
-            <Column column="TWW"
-                    title="Things that went well"
-                    dialog-title="Add something that went so well that we should do more of it"
-                    :ideas="thingsWentWell"
-                    @add-new-idea="addNewIdea"
-                    style="background-color: green">
-            </Column>
-            <Column column="TWNSW"
-                    title="Things that went not so well"
-                    dialog-title="Add something that went not so well that we should stop doing it"
-                    :ideas="thingsWentNotSoWell"
-                    @add-new-idea="addNewIdea"
-                    style="background-color: orangered">
-
-            </Column>
-            <Column column="FF"
-                    title="Feelings"
-                    dialog-title="Add a feeling that you have"
-                    :ideas="feelings"
-                    @add-new-idea="addNewIdea"
-                    style="background-color: dodgerblue">
-            </Column>
-
-          </v-row>
-      </v-container>
-    </v-main>
-
+    <AppMain />
     <AppFooter />
   </v-app>
 </template>
